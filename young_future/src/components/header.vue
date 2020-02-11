24d06c8822e6e47fa6eb06ae5aac5aa6fd4ee5ac
WARWINNER <55345224+WARWINNER@users.noreply.github.com>
2020-02-11 20:26:51 +0800

    young_future
    查询newspaper表,获取所有记录的title和uploadTime两个数据项

--- young_future/src/components/header.vue
+++ young_future/src/components/header.vue
@@ -1,25 +1,43 @@
 <template>
   <div class="header">
-     <div class="header_center">
-        <div class="logo-info">
-            <img src="@/assets/logo_black.png" alt="">
-        </div>
-        <div class="center">
-            <span class="site-page" @click="$router.push('/')"> 首页</span>
-            <span class="site-page" @click="$router.push({path:'/listpage',query:{type:'news'}});$store.state.listType='新闻'"> 新闻</span>
-            <span class="site-page" @click="$router.push({path:'/listpage',query:{type:'literature'}});$store.state.listType='文韵'"> 文韵</span>
-            <span class="site-page" @click="$router.push({path:'/listpage',query:{type:'newspaper'}});$store.state.listType='珞青报'"> 珞青报</span>
-            <span class="site-page" @click="$router.push({path:'/listpage',query:{type:'courier'}});$store.state.listType='最新速递'"> 最新速递</span>
-            <span class="site-page" @click="$router.push({path:'/listpage',query:{type:'handpicked'}});$store.state.listType='往期精选'"> 往期精选</span>
-            <span class="site-page" @click="$router.push({path:'/listpage',query:{type:'topic'}});$store.state.listType='专题'"> 专题</span>
-        </div>
-        <div class="right">
-             <el-input placeholder="请输入文章标题" v-model="titleName" class="searchClass">
-               <el-button slot="append" icon="el-icon-search"></el-button>
-            </el-input>
-        </div>
+    <div class="header_center">
+      <div class="logo-info">
+        <img src="@/assets/logo_black.png" alt />
       </div>
-   </div>
+      <div class="center">
+        <span class="site-page" @click="$router.push('/')">首页</span>
+        <span
+          class="site-page"
+          @click="$router.push({path:'/listpage',query:{type:'news'}});$store.state.listType='新闻'"
+        >新闻</span>
+        <span
+          class="site-page"
+          @click="$router.push({path:'/listpage',query:{type:'literature'}});$store.state.listType='文韵'"
+        >文韵</span>
+        <span
+          class="site-page"
+          @click="$router.push({path:'/listpage',query:{type:'newspaper'}});$store.state.listType='珞青报'"
+        >珞青报</span>
+        <span
+          class="site-page"
+          @click="$router.push({path:'/listpage',query:{type:'courier'}});$store.state.listType='最新速递'"
+        >最新速递</span>
+        <span
+          class="site-page"
+          @click="$router.push({path:'/listpage',query:{type:'handpicked'}});$store.state.listType='往期精选'"
+        >往期精选</span>
+        <span
+          class="site-page"
+          @click="$router.push({path:'/listpage',query:{type:'topic'}});$store.state.listType='专题'"
+        >专题</span>
+      </div>
+      <div class="right">
+        <el-input placeholder="请输入文章标题" v-model="titleName" class="searchClass">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -56,22 +74,23 @@ export default {
   watch:{
     '$store.state.listType':function(newFlag, oldFlag){
        console.log(newFlag,oldFlag);
+       this.$axios.get('/api/php/getNewspaper.php?type='+newFlag).then((res)=>{
+         console.log(res.data)})
        //更新列表数据
     }
   }
 }
 </script>
-
 <style lang='less'>
 @font-face {
-	font-family:SJ;
-	src: url('/static/font/SJ.TTF');
+  font-family: SJ;
+  src: url("/static/font/SJ.TTF");
 }
 .header {
   background-color: rgba(255, 255, 255, 1);
   height: 64px;
   width: 100%;
-  .header_center{ 
+  .header_center {
     height: 100%;
     display: flex;
     justify-content: space-between;
@@ -81,15 +100,15 @@ export default {
       color: #fff;
       height: 100%;
       img {
-          width: 130px;
-          margin-left: 20px;
-          margin-right: 20px;
-          vertical-align: middle;
+        width: 130px;
+        margin-left: 20px;
+        margin-right: 20px;
+        vertical-align: middle;
       }
     }
     .center {
       display: flex;
-      span{
+      span {
         cursor: pointer;
         position: relative;
         display: inline-block !important;
@@ -103,7 +122,7 @@ export default {
         text-shadow: none;
         font-size: 16px;
         z-index: 0;
-        font-family:   SJ;
+        font-family: SJ;
         font-weight: bold;
         -webkit-transition: all 0.2s;
         -moz-transition: all 0.2s;
@@ -116,61 +135,61 @@ export default {
         text-decoration: none;
       }
     }
-    .right{
+    .right {
       display: flex;
-      .searchClass{
-          border: 0.5px solid rgb(101,101,101);
-          border-radius: 50%;
-          background: rgb(250, 250, 250);
-          position: relative;
-          -ms-filter: none !important;
-          filter: none !important;
-          margin:auto 20px;
-          .el-input-group__append {
-            border: none;
-            background-color: transparent;
-          }
-          .el-input__inner {
-            width: 0px;
-            padding: 0px;
-            height: 36px;
-            line-height: 36px;
-            border: none;
-            background-color: transparent;
-          }
-          .el-icon-search{
-            font-size: 16px;
-            color: #409eff;
-          }
-          .el-button{
-            padding: 0
-          }
+      .searchClass {
+        border: 0.5px solid rgb(101, 101, 101);
+        border-radius: 50%;
+        background: rgb(250, 250, 250);
+        position: relative;
+        -ms-filter: none !important;
+        filter: none !important;
+        margin: auto 20px;
+        .el-input-group__append {
+          border: none;
+          background-color: transparent;
+        }
+        .el-input__inner {
+          width: 0px;
+          padding: 0px;
+          height: 36px;
+          line-height: 36px;
+          border: none;
+          background-color: transparent;
+        }
+        .el-icon-search {
+          font-size: 16px;
+          color: #409eff;
+        }
+        .el-button {
+          padding: 0;
+        }
       }
-      .searchClass:hover{
-        border: 0.5px solid rgb(101,101,101);
+      .searchClass:hover {
+        border: 0.5px solid rgb(101, 101, 101);
         border-radius: 20px;
         background: #fff;
-        .el-icon-search{
+        .el-icon-search {
           color: #409eff;
           font-size: 16px;
           font-weight: bold;
         }
         .el-input__inner {
-            width: 100%;
-            padding: 0 15px;
-            height: 36px;
-            line-height: 36px;
-            border: none;
-            background-color: transparent;
-            -webkit-transition: all .5s;
-            -moz-transition: all .5s;
-            transition: all .5s;
+          width: 100%;
+          padding: 0 15px;
+          height: 36px;
+          line-height: 36px;
+          border: none;
+          background-color: transparent;
+          -webkit-transition: all 0.5s;
+          -moz-transition: all 0.5s;
+          transition: all 0.5s;
         }
-        .el-button{
+        .el-button {
           padding: 12px 20px;
         }
       }
-    }    
+    }
   }
 }
 .site-page::after {
