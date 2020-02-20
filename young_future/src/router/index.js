531961b91a76b30e9d9457d1028324781d360b5b
2333174 <1741603127@qq.com>
2020-02-20 17:09:50 +0800

    router模式修改

--- young_future/src/router/index.js
+++ young_future/src/router/index.js
@@ -18,7 +18,7 @@ Router.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
 }
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   routes: [
     {
       path:'/article/:id',
