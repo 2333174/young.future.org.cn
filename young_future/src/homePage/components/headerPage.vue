7c7168069b314518d2998feabb54d7b15723fd3f
2333174 <1741603127@qq.com>
2020-02-24 17:33:32 +0800

    首页图片适配手机

--- young_future/src/homePage/components/headerPage.vue
+++ young_future/src/homePage/components/headerPage.vue
@@ -16,18 +16,26 @@ export default {
 <style lang="less">
 
 #nav {
-    background-image: url("/static/img/cover_future.png");
-    position: relative;
-    background-color: #49b1f5;
-    background-position: center;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    background-size: cover;
+  background-image: url("/static/img/cover_future.png");
+  position: relative;
+  background-color: #49b1f5;
+  background-position: center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  background-size: cover;
 }
 #nav.full_page {
-    background-attachment: local;
-    height: 100vh;
+  background-attachment: local;
+  height: 100vh;
 }
-
-
+@media (min-width:500px) {
+  #nav {
+    background-image: url("/static/img/cover_future.png");
+  }
+}
+@media (max-width:500px) {
+  #nav {
+    background-image: url("/static/img/phonebg.png");
+  }
+}
 </style>
